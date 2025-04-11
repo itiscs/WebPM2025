91c8c57569dbd6499d84909c04cca5afc167bac5
itiscs <moudroff@gmail.com>
2025-04-11 13:10:11 +0300

    Init

---
+++ FirstWebApp/FirstWebApp/Controllers/SecondController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Feedback()
+        {
+            return View();
+        }
+    }
+}
