47df297fc4c1557d7a2573eab1ac22f4c82621ed
itiscs <moudroff@gmail.com>
2025-05-23 09:18:28 +0300

    Identity

---
+++ IdentityApp25/IdentityApp25/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using IdentityApp25.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp25.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
